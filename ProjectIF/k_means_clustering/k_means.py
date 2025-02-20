9a7498eddcf96030da2e8dcb67efa8e987c52211
WenYan <105508730+GreenBam@users.noreply.github.com>
2025-02-19 17:45:30 -0800

    added kmeans

---
+++ ProjectIF/k_means_clustering/k_means.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+import argparse
+
+# Read in the image
+file_name = os.path.join(os.path.dirname(__file__), 'plant.jpg')
+assert os.path.exists(file_name)
+
+image = cv2.imread(file_name)
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Reshaping the image into a 2D array of pixels and 3 color values (RGB)
+pixel_vals = image.reshape((-1,3))
+
+# Convert to float type
+pixel_vals = np.float32(pixel_vals)
+
+#the below line of code defines the criteria for the algorithm to stop running, 
+#which will happen is 100 iterations are run or the epsilon (which is the required accuracy) 
+#becomes 85%
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
+
+# then perform k-means clustering with number of clusters defined as 3
+#also random centres are initially choosed for k-means clustering
+k = 5
+retval, labels, centers = cv2.kmeans(pixel_vals, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+# convert data into 8-bit values
+centers = np.uint8(centers)
+segmented_data = centers[labels.flatten()]
+
+# reshape data into the original image dimensions
+segmented_image = segmented_data.reshape((image.shape))
+
+plt.imshow(segmented_image)
+plt.show()
